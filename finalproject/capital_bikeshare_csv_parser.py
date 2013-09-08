c23fed2687bde82e6e933e2288fe74a093326e8d
Kevin Perko <kperko@smartcoffeetech.com>
2013-09-07 19:44:42 -0700

    add columns and indexes to *.sql, finalize script, turn on db inserts to *.py

--- finalproject/capital_bikeshare_csv_parser.py
+++ finalproject/capital_bikeshare_csv_parser.py
@@ -1,85 +1,134 @@
+print 'importing modules'
 import csv
 import MySQLdb
-import config as cfg
+from datetime import datetime
+import re
+#import config as cfg
 
 #can we find out the price? it could be interesting to chart money trends.
-
-def get_bike_data():
-	#get the csv data
-	base_url='http://www.capitalbikeshare.com/assets/files/trip-history-data/'
-	ext = '.csv'
-
-	url_list = ['2010-4th-quarter','2011-1st-quarter','2011-2nd-quarter','2011-3rd-quarter','2011-4th-quarter', 
-	'2012-1st-quarter','2012-2nd-quarter','2012-3rd-quarter','2012-4th-quarter','2013-1st-quarter','2013-2nd-quarter']
-
-	#convert duration to HH:MM:SS
+		
+#YYYY-MM-DD
+def insert_data(con,cur,rental_list):
+	""" inserts the bike share data into a db """
 	
-	#example row
-	#['0h 28min. 26sec.', '11/21/2010 13:56', '11/21/2010 14:24', 'Georgia & New Hampshire Ave NW (31400)', 'Massachusetts Ave & Dupont Circle NW (31200)', 'W00830', 'Casual']
-	#rental_duration,checkout_time,return_time,rental_location,return_location,bike_number,rider_type
+	qry = """ INSERT INTO bike_rentals (trip_duration,start_date,end_date,start_station,start_terminal,end_station,
+	end_terminal,bike_num,member_type)
+	values('%s','%s','%s',"%s",'%s',"%s",'%s','%s','%s') """ % (rental_list[0],rental_list[1], \
+	rental_list[2],rental_list[3],rental_list[4],rental_list[5],rental_list[6],rental_list[7],rental_list[8])
 
-	rental_duration_list = []
-
-	for each_url in url_list:
-		csv_url = base_url+each_url+ext
-		response = urllib2.urlopen(csv_url).read()
-		output = StringIO.StringIO(response)
-		data = csv.reader(output)
+	cur.execute(qry)
+	con.commit()
 	
-		for each_row in data:
-			rental_duration_list.append(each_row[0])
-		
-		
-		print 'number of records', len(rental_duration_list)
-		
 
-		
-def parse_csv(filename):
-	num_rentals = 0
+def date_fmt(date_str):
+	"""input a date string and returns a datetime object"""
+	
+	pos = [n.start() for n in re.finditer(r"/",date_str)]
+	space = date_str.find(' ')
+	colon = date_str.find(':')
+	
+	date_obj = datetime(int(date_str[pos[1]+1:pos[1]+5]),int(date_str[0:pos[0]]),int(date_str[pos[0]+1:pos[1]]),int(date_str[space+1:colon]),int(date_str[colon+1:]))
+	#date_obj = datetime(int(date_str[6:10]),int(date_str[0:char_pos_list[0]]),int(date_str[3:5]),int(date_str[11:13]),int(date_str[14:16]))
+	
+	return date_obj
+
+
+def terminal_fmt(station):
+	""" extracts and removes the termainl from a station str"""
+	
+	terminal = station[station.find('(')+1:station.find(')')]
+	station = station[::-1][8:][::-1]
+	
+	return station,terminal
+
+
+def parse_csv(con,cur,filename):
+	""" input a csv, parse it, process it, return consistent data for a db insert"""
+	
 	with open(filename) as f:
 		reader = csv.reader(f, delimiter=',')
+		reader.next()
 		for each_line in reader:
-			num_rentals +=1
-			#print each_line
-		"""	print 'trip_duration', each_line[0]
+			"""print 'trip_duration', each_line[0]
 			print 'start_date', each_line[1]
 			print 'end_date', each_line[2]
 			print 'start_station', each_line[3]
 			print 'end_station', each_line[4]
 			print 'bike_num', each_line[5]
 			print 'member_type', each_line[6]"""
-			return each_line[0],each_line[1],each_line[2],each_line[3],each_line[4],each_line[5],each_line[6]
-	#return num_rentals
-	
+			'1/1/2011 0:00'
+			
+			each_line = [each.strip() for each in each_line]
+			
+			#parsing logic for inconsistent csv structure
+			# <3 data inconsistency
+			try:
+				if filename[70:] in ['2012-1st-quarter.csv','2012-2nd-quarter.csv']:
+					start_date = date_fmt(each_line[2])
+					end_date = date_fmt(each_line[5])
+				
+					line = [each_line[0],start_date,end_date,each_line[3],each_line[4],each_line[6],each_line[7],each_line[8],each_line[9]]
+					#return line
+					insert_data(con,cur,line)
+			
+				elif filename[70:] in ['2010-4th-quarter.csv','2011-1st-quarter.csv','2011-2nd-quarter.csv','2011-3rd-quarter.csv','2011-4th-quarter.csv']:
+					start_date = date_fmt(each_line[1])
+					end_date = date_fmt(each_line[2])
+					start_station,start_terminal = terminal_fmt(each_line[3])
+					end_station,end_terminal = terminal_fmt(each_line[4])
+				
+					line = [each_line[0],start_date,end_date,start_station,start_terminal,end_station,end_terminal,each_line[5],each_line[6]]
+					#return line
+					insert_data(con,cur,line)
+					
+				else:
+					start_date = date_fmt(each_line[1])
+					end_date = date_fmt(each_line[4])
+				
+					line = [each_line[0],start_date,end_date,each_line[2],each_line[3],each_line[5],each_line[6],each_line[7],each_line[8]]
+					#return line
+					insert_data(con,cur,line)
+					
+			except Exception, ie:
+					print 'houston, we have a problem: ', ie
+					print 'filename: ', filename
+					print 'data: ', each_line
+					
+		#return line
+
+
 def init_db():
-	con = MySQLdb.connect(host=cfg.host,user=cfg.user,pwd=cfg.pwd,port=3306)
+	""" instantiate and return the db and cur objects"""
+	
+	#con = MySQLdb.connect(host=cfg.host,user=cfg.user,passwd=cfg.pwd,db=cfg.db,port=3306)
+	con = MySQLdb.connect(user='',passwd='',db='test')
 	cur = con.cursor()
 	
 	return con,cur
-	
-def insert_into_db(*args):
-	qry = """ insert into"""
-	
-	sql.
+
 
 def main():
-	#lsetup the dir struct
+	
+	#runtime: ~24 mins for 4.1M records
 	#list of filenames for iteration - could use os module for this
 	dir_ = "/Users/kperko/work/data-science-class/GADataScience/finalproject/data/"
 	filename = ['2010-4th-quarter','2011-1st-quarter','2011-2nd-quarter','2011-3rd-quarter','2011-4th-quarter', 
 	'2012-1st-quarter','2012-2nd-quarter','2012-3rd-quarter','2012-4th-quarter','2013-1st-quarter','2013-2nd-quarter']
 	ext = '.csv'
 	
+	try:
 	#connect to the db and instantiate a connection and cursor obj
-	con,cur = init_db()
-	
-	total_rentals = 0
-	for each_file in filename:
-		each_file_path = dir_+each_file+ext
-		insert_into_db(parse_csv(each_file_path))
-		#print each_file, rentals
-		#total_rentals += rentals
-	#print 'total_rentals', total_rentals
-	
+		con,cur = init_db()
+		
+		print 'loading data'
+		
+		for each_file in filename:
+			each_file_path = dir_+each_file+ext
+			parse_csv(con,cur,each_file_path)
+			#insert_data(con,cur,parse_csv(each_file_path))
+			
+	except Exception, exc:
+		print 'exception issued: ', exc
+
 if __name__ == "__main__":
 	main()
--- finalproject/create-bike_rentals.sql
+++ finalproject/create-bike_rentals.sql
@@ -1,21 +1,21 @@
+drop table if exists bike_rentals;
 create table bike_rentals (
 	row_id int not null auto_increment,
-	trip_duration char(8) default null,#HH:MM:SS
+	trip_duration varchar(100) default null,#HH:MM:SS
 	start_date datetime default null,
 	end_date datetime default null,
 	start_station varchar(255) default null,
+	start_terminal varchar(20) default null,
 	end_station varchar(255) default null,
+	end_terminal varchar(20) default null,
 	bike_num varchar(20) default null,
 	member_type varchar(10), #Registered - annual or Monthly or Casual 1 to 5 day member
-	primary key(row_id),
-	index(duration),
-	index(start_date),
-	index(end_date),
-	index(start_station),
-	index(end_station),
-	index(member_type) 
+	primary key(row_id)
 	);
-	
+
+-- Add INDEXES AFTER INSERT
+ALTER TABLE bike_rentals add index (trip_duration,start_date,end_date,start_station,start_terminal,end_station,end_terminal,bike_num,member_type);
+ALTER TABLE bike_rentals add index (start_date);
 
 drop table if exists weather_data;
 create table weather_data (
@@ -40,3 +40,15 @@ create table weather_data (
 	date_created datetime DEFAULT CURRENT_TIMESTAMP,
 	date_modified datetime default null
 	);
+	
+-- add indexes after insert
+ALTER TABLE weather_data add index(icon);
+ALTER TABLE weather_data add index(conds);
+ALTER TABLE weather_data add index(tempi);
+ALTER TABLE weather_data add index(fog);
+ALTER TABLE weather_data add index(rain);
+ALTER TABLE weather_data add index(hail);
+ALTER TABLE weather_data add index(thunder);
+ALTER TABLE weather_data add index(tornado);
+ALTER TABLE weather_data add index(date_recorded);
+ALTER TABLE weather_data add index(hour_recorded);
